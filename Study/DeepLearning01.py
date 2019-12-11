967628f5b03f24f96d2614a3404054a671b035c9
Heechul90 <heechul4296@gmail.com>
2019-12-11 17:39:18 +0900

    딥러닝 학습

--- Study/DeepLearning01.py
+++ Study/DeepLearning01.py
@@ -14,19 +14,32 @@ tf.set_random_seed(seed)
 # 준비된 수술 환자 데이터를 불러옴
 data_set = np.loadtxt('dataset1/ThoraricSurgery.csv',
                       delimiter = ',')
+data_set
+data_set.shape
+
 
 # 환자의 기록과 수술 결과를 X와 Y로 구분
 X = data_set[:, 0:17]
 Y = data_set[:, 17]
 
-# 딥러닝 구조를 결정(모델을 설정하고 실행하는 부분)
-model = Sequential()
-model.add(Dense(30, input_dim = 17, activation = 'relu'))
-model.add(Dense(1, activation = 'sigmoid'))
 
-# 딥러닝을 실행
+# 딥러닝 구조를 결정(모델을 설정하고 실행하는 부분)
+# 딥러닝은 퍼셉트론 위에 숨겨진 퍼셉트론 층을 차곡차곡 추가하는 형태
+# 층들을 케라스에서 Sequential() 함수를 통해서 구현
+# model.add를 통해서 라인(층)을 추가
+# Dence 함수를 통해서 구체적으로 구조를 정함
+
+model = Sequential()                                       # Sequential() 함수를 model로 선언
+model.add(Dense(30, input_dim = 17, activation = 'relu'))  # model.add로 층을 추가, Dense 함수로 30개의 노드생성
+model.add(Dense(1, activation = 'sigmoid'))                #
+
+
+# 딥러닝 컴파일
 model.compile(loss = 'binary_crossentropy',
               optimizer = 'adam',
               metrics = ['accuracy'])
 
+# 모델 실행
+# epochs: 학습 프로세스가 모든 샘플에 대해 한 번 실행되는 것을 1epoch
+# batch_size: 샘플을 한번에 몇 개씩 처리할지를 정하는 부분
 model.fit(X, Y, epochs = 30, batch_size = 10)
--- Study/DeepLearning02.py
+++ Study/DeepLearning02.py
@@ -1,17 +1,75 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+# 딥러닝을 구동하는 데 필요한 케라스 함수를 불러옴
+from keras.models import Sequential
+from keras.layers import Dense
 
+# 필요한 라이브러리를 불러옴
+import numpy as np
+import tensorflow as tf
+import pandas as pd
 
 # 데이터 불러오기
-raw_data = pd.read_csv('data/temperature.csv')
-raw_data
+raw_data = pd.read_csv('dataset1/pima-indians-diabetes.csv',
+                       names = ['pregnant', 'plasma', 'pressure', 'thickness',
+                                'insulin', 'BMI', 'pedigree', 'age', 'class'])
+raw_data.head()
 
 data = raw_data.copy()
 data.columns
 data.head()
+data.info()
+data.describe()
+data[['pregnant', 'class']]
 
-data = data.set_index('time')
-data.head()
+
+data[['pregnant', 'class']].groupby(['pregnant'], as_index = False).mean().sort_values(by = 'pregnant', ascending = True)
 
 
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.figure(figsize = (12, 12))
+sns.heatmap(data.corr(), linewidths = 0.1, vmax = 0.5, cmap = plt.cm.gist_heat, linecolor = 'white', annot = True)
+plt.show()
+
+grid = sns.FacetGrid(data, col = 'class')
+grid.map(plt.hist, 'plasma', bins = 10)
+plt.show()
+
+
+
+#######################################################################
+# seed 값 생성
+seed = 0
+np.random.seed(seed)
+tf.set_random_seed(seed)
+
+# 데이터 불러오기
+dataset = np.loadtxt('dataset1/pima-indians-diabetes.csv',
+                     delimiter = ',')
+
+dataset.shape
+X = dataset[:, 0:8]
+Y = dataset[:, 8]
+
+
+# 모델 설정
+# 입력층 8
+# 은닉층 노드 12개, 활성화 함수: 렐루
+# 은닉층 노드 8개, 활성화 함수: 렐루
+# 출력층 노드 1개, 활성화 함수: 시그모이드
+model = Sequential()
+model.add(Dense(12, input_dim = 8, activation = 'relu'))
+model.add(Dense(8, activation = 'relu'))
+model.add(Dense(1, activation = 'sigmoid'))
+
+# 모델 컴파일
+model.compile(loss = 'binary_crossentropy',
+              optimizer = 'adam',
+              metrics = ['accuracy'])
+
+# 모델 실행
+model.fit(X, Y, epochs = 200, batch_size = 10)
+
+# 결과 출력
+print("\n Accuracy: %.4f" % (model.evaluate(X, Y)[1]))
